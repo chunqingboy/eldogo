72e46f3a082786ab415c7ca4cdcf746f8a35c601
Koihin Wong <monkeylikeaboy@gmail.com>
2023-05-08 10:06:42 +0800

    Final commit

--- app/src/main/java/com/raywenderlich/android/eldogo/DogDetailsFragment.kt
+++ app/src/main/java/com/raywenderlich/android/eldogo/DogDetailsFragment.kt
@@ -35,6 +35,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.raywenderlich.android.eldogo.databinding.FragmentDogDetailsBinding
+
 
 //1
 class DogDetailsFragment : Fragment() {
@@ -42,16 +44,33 @@ class DogDetailsFragment : Fragment() {
   //2
   companion object {
 
-    fun newInstance(): DogDetailsFragment {
-      return DogDetailsFragment()
+    private const val DOGMODEL = "model"
+
+    fun newInstance(dogModel: DogModel): DogDetailsFragment {
+      val args = Bundle()
+      args.putSerializable(DOGMODEL, dogModel)
+      val fragment = DogDetailsFragment()
+      fragment.arguments = args
+      return fragment
     }
+
   }
 
   //3
   override fun onCreateView(inflater: LayoutInflater,
                             container: ViewGroup?,
                             savedInstanceState: Bundle?): View? {
-    return inflater.inflate(R.layout.fragment_dog_details, container, false)
+    // 1
+    val fragmentDogDetailsBinding =
+      FragmentDogDetailsBinding.inflate(inflater, container, false)
+
+    // 2
+    val model = arguments!!.getSerializable(DOGMODEL) as DogModel
+    // 3
+    fragmentDogDetailsBinding.dogModel = model
+    model.text = String.format(getString(R.string.description_format), model.description, model.url)
+    return fragmentDogDetailsBinding.root
+
   }
 
 }
--- app/src/main/java/com/raywenderlich/android/eldogo/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/eldogo/MainActivity.kt
@@ -32,11 +32,30 @@ package com.raywenderlich.android.eldogo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), DogListFragment.OnDogSelected {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.root_layout, DogListFragment.newInstance(), "dogList")
+                .commit()
+        }
     }
 
+    override fun onDogSelected(dogModel: DogModel) {
+        val detailsFragment =
+            DogDetailsFragment.newInstance(dogModel)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.root_layout, detailsFragment, "dogDetails")
+            .addToBackStack(null)
+            .commit()
+    }
 }
+
